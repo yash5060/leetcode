98bdb244d7bffed33da482dab1fa4000dbecd85b
yash5060 <98175266+yash5060@users.noreply.github.com>
2022-01-24 22:29:51 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.00%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        int n=word.size();
+        
+        
+        for(int i=1;i<n;i++)
+        {
+            if(isupper(word[1])!=isupper(word[i])|| (islower(word[0]) && isupper(word[i])))
+               {
+                   return false;
+               }
+               
+        }
+       return true;
+        
+        
+    }
+};
