1dc362c300f7974f80fb50d69e147a30a39e067d
Yash <98175266+yash5060@users.noreply.github.com>
2022-06-08 17:11:39 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removePalindromeSub(string s) {
       
-      if(s.size()==0)return 0;
+     
       string y=s;
       reverse(s.begin(), s.end());
       if(s==y) return 1;
