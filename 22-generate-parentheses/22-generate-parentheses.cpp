fb5895f5a89085271fe9c7cbeada8d74167eedf3
Yash <98175266+yash5060@users.noreply.github.com>
2022-07-13 21:47:22 +0530

    Time: 3 ms (86.63%), Space: 11.2 MB (90.49%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,37 +1,41 @@
-void solver(int n1,int n2,string &s,vector<string>&ans)
-{
-    if(n1==0 &&n2==0)
-    {
-        ans.push_back(s);
-        return;
-    }
-    if(n1>0)
-    {
-        s.push_back('(');
-        solver(n1-1,n2,s,ans);
-        s.pop_back();
-        
-    }
-    if(n2>0)
-    {
-        if(n1<n2)//yha pe n1<n2 aayega n ki n2<n1 kuki ye define kr rha h bachi hui parenthesis
-        {
-            s.push_back(')');
-            solver(n1,n2-1,s,ans);
-            s.pop_back();
-        }
-    }
-    
-}
-
 class Solution {
 public:
+  
+  void solver(int open,int close,vector<string> &ans,string &an)
+  {
+    //base case
+    if(open==0 && close==0)
+    {
+      ans.push_back(an);
+      return;
+    }
+    
+    
+    //opening
+    if(open>0)
+    {
+      an.push_back('(');
+      solver(open-1,close,ans,an);
+      an.pop_back();
+    }
+    
+    //closing
+    if(close>0 && close>open)
+    {
+       an.push_back(')');
+      solver(open,close-1,ans,an);
+      an.pop_back();
+
+    }
+    
+    
+  }
     vector<string> generateParenthesis(int n) {
-        
-        string s;
-        vector<string>ans;
-        solver(n,n,s,ans);
-        return ans;
+      
+      vector<string>ans;
+      string an="";
+      solver(n,n,ans,an);
+      return ans;
         
     }
 };
