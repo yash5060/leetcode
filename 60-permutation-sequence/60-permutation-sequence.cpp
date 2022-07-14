bfd85718437e904f2448f11a055c83ddb917cab1
Yash <98175266+yash5060@users.noreply.github.com>
2022-07-14 23:04:51 +0530

    Time: 289 ms (9.72%), Space: 5.8 MB (99.37%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
-    vector<int> fact;
-    
-    void findfact(int n)
-    {
-        fact = vector<int>(n);
-        
-        fact[0] = 1;
-        
-        if(n==1) return;
-        
-        fact[1] = 1;
-        for(int i=2; i<n; i++)
-            fact[i] = i*fact[i-1];
-    }
-    
-    string getPermutation(int N, int K) {
-        int n = N-1, k = K-1, nt, kt;
-        
-        findfact(N);
-        vector<int> num(N);
-        for(int i=0; i<N; i++)
-            num[i] = i+1;
-        vector<int>::iterator it; 
-        
-        string ans = "";
-        
-        while(n>=0)
+    string getPermutation(int n, int k) {
+        string s;
+        for(int i=1;i<=n;i++)
         {
-            nt = k/fact[n];
-            kt = k%fact[n];
-
-            ans += (num[nt]+'0');
-            it = num.begin(); 
-            num.erase(it+nt);
-            n--;
-            k = kt;   
+            s.push_back('0'+i);
         }
-
-        return ans;
+        
+        k-=1;
+        while(k--)
+        {
+            next_permutation(s.begin(),s.end());
+        }
+        return s;
     }
 };
