bfe5bafe0a156dc712d460608d797e5e4a686dbe
Yash <98175266+yash5060@users.noreply.github.com>
2022-08-03 13:24:50 +0530

    Time: 30 ms (83.20%), Space: 27.7 MB (68.42%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -12,32 +12,24 @@
 class Solution {
 public:
   
-  int solve(TreeNode *root,int &maxi)
+  
+  int solver(TreeNode *root,int &maxi)
   {
     if(root==NULL)
-    {
       return 0;
-    }
-    int l=max(0,solve(root->left,maxi));
-    int r=max(0,solve(root->right,maxi));
     
-   
+    int l=max(0,solver(root->left,maxi));
+    int r=max(0,solver(root->right,maxi));
+    
     maxi=max(maxi,root->val+l+r);
     
     return root->val+max(l,r);
-    
   }
+  
     int maxPathSum(TreeNode* root) {
       
-      int r=root->val;
-      
-      
       int maxi=-9999;
-      int x=solve(root,maxi);
-      if(r>maxi)
-      {
-        return x;
-      }
+      solver(root,maxi);
       return maxi;
         
     }
