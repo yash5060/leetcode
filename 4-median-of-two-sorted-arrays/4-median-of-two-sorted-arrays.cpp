2e3287f0a66ebc5c0a698ade71cc53213293294a
Yash <98175266+yash5060@users.noreply.github.com>
2022-09-27 10:15:29 +0530

    Time: 141 ms (5.03%), Space: 90.1 MB (24.50%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,77 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+      
+      int s1=nums1.size();
+      int s2=nums2.size();
+      
+      // int sz=min(s1,s2);
+       vector<int>ans;
+      
+      int p1=0;
+      int p2=0;
+      
+      if(s1==0){
+        ans=nums2;
         
-        int s1=nums1.size();
-        int s2=nums2.size();
-        
-        vector<int>x (s1+s2);
-        int i=0;
-        
-        for( i=0;i<s1;i++)
+      }
+      else if(s2==0)
+      {
+        ans=nums1;
+      }
+         
+      
+     
+      else{
+      for(int i=0;i<s1+s2;i++)
+      {
+        if(nums1[p1]<nums2[p2])
         {
-            x[i]=nums1[i];
+          ans.push_back(nums1[p1]);
+          p1++;
+        }
+        else{
+          ans.push_back(nums2[p2]);
+          p2++;
         }
         
-        for(int j=0;j<s2;j++)
+        if(p1==s1||p2==s2)
         {
-            x[i]=nums2[j];
-            i++;
+          break;
         }
-        
-        sort(x.begin(),x.end());
-        
-        double med;
-        
-        if(x.size()%2==0)
-        {
-            int m=x.size();
-            double f1=x[m/2];
-            double f2=x[(m/2)-1];
-            med=(f1+f2)/2;
-        }
-        
-        else if(x.size()%2!=0)
-        {
-            med=x[x.size()/2];
-        }
-        
-        return med;
-        
+      }
+      
+      for(int i=p1;i<s1;i++)
+      {
+        ans.push_back(nums1[i]);
+      }
+      for(int i=p2;i<s2;i++)
+      {
+        ans.push_back(nums2[i]);
+      }
+      }
+      
+      for(int i=0;i<ans.size();i++)
+      {
+        cout<<ans[i]<<" ";
+      }
+      float an;
+      
+      int sz=ans.size();
+      
+      if(sz%2==0)
+      {
+        int x=sz/2;
+        int p1=ans[x];
+        cout<<p1<<endl;
+        int p2=ans[(x-1)];
+        cout<<p2<<endl;
+        int sm=p1+p2;
+         an=sm/2.0;
+      }
+      else{
+        an=ans[sz/2];
+      }
+        return an;
     }
 };
